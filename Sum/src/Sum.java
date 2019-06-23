7af4e75166487bc5268dc7c8d20afa2d58ba6edb
SDEdison189 <edisons@seas.upenn.edu>
2019-06-23 10:14:35 -0400

    made the input prompt slightly more descriptive

--- Sum/src/Sum.java
+++ Sum/src/Sum.java
@@ -8,7 +8,7 @@ public class Sum
    public static void main(String[] args)
    {  
 		Scanner num = new Scanner(System.in);  // Create a Scanner object
-	    System.out.println("Please give a number");
+	    System.out.println("This program can tell you the sum of all integers between 0 and a number you select. Please give a number:");
 	    int n = num.nextInt();
 	    int sum = 0;
 	    for (int i = 1; i <= n; i++) {
