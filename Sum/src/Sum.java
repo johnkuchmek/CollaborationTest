0c1fac34be63152a6322aee5d7e6cc82161369b0
bxlalberto <52110082+bxlalberto@users.noreply.github.com>
2019-06-23 21:06:26 +0200

    Update Sum.java

--- Sum/src/Sum.java
+++ Sum/src/Sum.java
@@ -8,10 +8,10 @@ public class Sum
    public static void main(String[] args)
    {  
 		Scanner num = new Scanner(System.in);  // Create a Scanner object
-	    System.out.println("This program can tell you the sum of all integers between 0 and a number you select. Please give a number:");
+	    System.out.println("This program can tell you the sum of every 2nd integer between 0 and a number you select. Please give a number:");
 	    int n = num.nextInt();
 	    int sum = 0;
-	    for (int i = 1; i <= n; i++) {
+	    for (int i = 1; i <= n; i=i+2) {
 	    	sum = sum + i;
 	    }
 	    System.out.println(sum);
